1e7b92352167fb466fddbf1503be9e0af0648e5f
Jacob Cochrane <104926747+JarJarDinks@users.noreply.github.com>
2023-01-07 20:34:24 -0700

    added and array with 3 objects

--- simple-js-app/js/scripts.js
+++ simple-js-app/js/scripts.js
@@ -1,4 +1,22 @@
-alert('Hello World');
+let pokemonList = [pokemon1, pokemon2, pokemon3];
 
-let favoriteFood = 'Tacos';
-document.write(favoriteFood);
+let pokemon1 = {
+    name: 'Staraptor',
+    type: ['Flying', 'Normal'],
+    weight: 24.9, 
+    unit: 'kg'
+};
+
+let pokemon2 = {
+    name: 'Jigglypuff', 
+    type: ['Fairy', 'Normal'], 
+    weight: 5.5, 
+    unit: 'kg'
+};
+
+let pokemon3 = {
+    name: 'Onix', 
+    type: ['Rock','Ground'], 
+    weight: 210,
+    unit: 'kg'
+};
