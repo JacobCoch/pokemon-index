529e5bf93a94cc9e0dc2cccbb2adb6d96a965c09
Jacob Cochrane <104926747+JarJarDinks@users.noreply.github.com>
2023-01-10 21:14:40 -0700

    implemented a function to write both lists onto the DOM

--- simple-js-app/js/scripts.js
+++ simple-js-app/js/scripts.js
@@ -1,34 +1,69 @@
-// object 1
-let pokemon1 = {
-    name: 'Staraptor',
-    type: ['Flying', 'Normal'],
-    weight: 24.9, 
-    unit: 'kg'
-};
-// object 2
-let pokemon2 = {
-    name: 'Jigglypuff', 
-    type: ['Fairy', 'Normal'], 
-    weight: 5.5, 
-    unit: 'kg'
-};
-// object 3
-let pokemon3 = {
-    name: 'Onix', 
-    type: ['Rock','Ground'], 
-    weight: 210,
-    unit: 'kg'
-};
 //this is an array
-let pokemonList = [pokemon1, pokemon2, pokemon3];
-
-// a for loop that writes the pokemon and their weight on the DOM
-for (let i = 0; i < pokemonList.length; i++ ){
-    let pokemon = pokemonList[i];
-    document.write (pokemon.name + ": " + " (" + pokemon.weight + " " + pokemon.unit + ") ");
-    if (pokemon.weight > 100){
-        document.write ("Wow, that's big! ")
+let pokemonList = [
+    pokemon1 = {
+        name: 'Staraptor',
+        type: ['Flying', 'Normal'],
+        weight: 24.9, 
+        unit: 'kg'
+    },
+    // object 2
+    pokemon2 = {
+        name: 'Jigglypuff', 
+        type: ['Fairy', 'Normal'], 
+        weight: 5.5, 
+        unit: 'kg'
+    },
+    // object 3
+    pokemon3 = {
+        name: 'Onix', 
+        type: ['Rock','Ground'], 
+        weight: 210,
+        unit: 'kg'
+    },
+];
+// this is the 2nd array
+let pokemonList2 = [
+    pokemon4 = {
+        name: 'Charmander',
+        type:['fire'],
+        weight: 8.5,
+        unit: 'kg'
+    },
+    pokemon4 = {
+        name: 'Squirtle',
+        type:['water'],
+        weight: 9,
+        unit: 'kg'
+    },
+    pokemon4 = {
+        name: 'Beedrill',
+        type:['bug', 'poison'],
+        weight: 29.5,
+        unit: 'kg'
     }
-    // this creates a line break after each object
-    document.write("<br>")
+]
+
+// this is a function
+function printArrayDetails(list){
+    for (let i = 0; i < list.length; i++){
+        let pokemon = list[i];
+        document.write(pokemon.name + ": " + " (" + pokemon.weight + " " + pokemon.unit + ") ");
+        if (pokemon.weight > 25){ 
+            document.write (" -" + "Wow, that's big!")
+        }
+        document.write("<br>")
+    }
+}
+
+printArrayDetails(pokemonList);
+printArrayDetails(pokemonList2);
+
+
+function div(dividend, divisor){
+    if (divisor === 0) {
+        return "You're trying to divide by zero."
+    } else{
+        let result = dividend/divisor;
+        return result;
+    }
 }
