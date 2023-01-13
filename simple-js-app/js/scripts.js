9da5fd261e07f61c7cd910c8a7e62ee103314eb2
Jacob Cochrane <104926747+JarJarDinks@users.noreply.github.com>
2023-01-12 20:42:30 -0700

    added an IIFE

--- simple-js-app/js/scripts.js
+++ simple-js-app/js/scripts.js
@@ -1,55 +1,46 @@
-//this is an array
-let repositoryOne = [
-    // object1
-    Staraptor = {
-        name: 'Staraptor',
-        type: ['Flying', 'Normal'],
-        weight: 24.9, 
-        unit: 'kg'
-    },
-    // object 2
-    Jigglypuff = {
-        name: 'Jigglypuff', 
-        type: ['Fairy', 'Normal'], 
-        weight: 5.5, 
-        unit: 'kg'
-    },
-    // object 3
-    Onix = {
-        name: 'Onix', 
-        type: ['Rock','Ground'], 
-        weight: 210,
-        unit: 'kg'
-    },
-];
-// this is the 2nd array
-let repositoryTwo = [
-    Charmander = {
-        name: 'Charmander',
-        type:['fire'],
-        weight: 8.5,
-        unit: 'kg'
-    },
-    Squirtle = {
-        name: 'Squirtle',
-        type:['water'],
-        weight: 9,
-        unit: 'kg'
-    },
-    Beedrill = {
-        name: 'Beedrill',
-        type:['bug', 'poison'],
-        weight: 29.5,
-        unit: 'kg'
+    
+let pokemonRepository = (function() {
+    let pokemonList = [
+        // object1
+        {
+            name: 'Staraptor',
+            type: ['Flying', 'Normal'],
+            weight: 24.9, 
+            unit: 'kg'
+        },
+        // object 2
+        {
+            name: 'Jigglypuff', 
+            type: ['Fairy', 'Normal'], 
+            weight: 5.5, 
+            unit: 'kg'
+        },
+        // object 3
+        {
+            name: 'Onix', 
+            type: ['Rock','Ground'], 
+            weight: 210,
+            unit: 'kg'
+        },
+    ];
+    return {
+        getAll: function() {
+            return pokemonList;
+        },
+        add: function(item) {
+            pokemonList.push(item);
+        }
     }
-]
+})();
 
-function loopFunction(user) {
-    document.write(user.name + ": " + user.weight + "(" + user.unit + ") ");
-    if (user.weight > 25) {
+console.log(pokemonRepository.getAll());
+
+
+function loopFunction(pokemon) {
+    document.write(pokemon.name + ": " + pokemon.weight + "(" + pokemon.unit + ") ");
+    if (pokemon.weight > 25) {
         document.write(" -" + "Wow, that's big!")
     }
     document.write("<br>")
 } 
-repositoryOne.forEach(loopFunction);
-repositoryTwo.forEach(loopFunction);
+pokemonRepository.getAll().forEach(loopFunction);
