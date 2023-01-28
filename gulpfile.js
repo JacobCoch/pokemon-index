58fad747b0d53cf5e918f9daa45217f80b999f37
JarJarDinks <cochranejacob22@gmail.com>
2023-01-28 16:26:22 -0700

    added airbnb Eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
-const { src, dest, watch, series, parallel } = require('gulp');
+const {
+  src, dest, watch, series, parallel,
+} = require('gulp');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const postcss = require('gulp-postcss');
@@ -6,29 +8,28 @@ const replace = require('gulp-replace');
 const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass')(require('sass'));
 const uglify = require('gulp-uglify');
-const concat = require('gulp-concat')
+const concat = require('gulp-concat');
 const browserSync = require('browser-sync').create();
 
 // File Path Variables
-const files ={
-    scssPath: 'src/scss/**/*.scss',
-    jsPath: 'src/js/**/*.js'
-}
+const files = {
+  scssPath: 'src/scss/**/*.scss',
+  jsPath: 'src/js/**/*.js',
+};
 
 // Sass task
 function scssTask() {
-    return src(files.scssPath)
+  return src(files.scssPath)
     .pipe(sourcemaps.init())
     .pipe(sass())
-    .pipe(postcss([ autoprefixer(), cssnano() ]))
+    .pipe(postcss([autoprefixer(), cssnano()]))
     .pipe(sourcemaps.write('.'))
-    .pipe(dest('dist')
-    );
+    .pipe(dest('dist'));
 }
 
 // JS task
 function jsTask() {
-    return src(files.jsPath)
+  return src(files.jsPath)
     .pipe(sourcemaps.init())
     .pipe(concat('all.js'))
     .pipe(uglify())
@@ -37,13 +38,15 @@ function jsTask() {
 }
 
 // Watch task
-function watchTask(){
-    watch([files.scssPath, files.jsPath],
-        parallel(scssTask, jsTask));
+function watchTask() {
+  watch(
+    [files.scssPath, files.jsPath],
+    parallel(scssTask, jsTask),
+  );
 }
 
-//Default task
+// Default task
 exports.default = series(
-    parallel(scssTask, jsTask),
-    watchTask
+  parallel(scssTask, jsTask),
+  watchTask,
 );
